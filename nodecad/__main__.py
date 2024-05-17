34b577b3e10f4b6d6eb53a44a3fb16b1f0e7e79a
“HakanSeven12” <hakanseven12@gmail.com>
2024-05-17 05:26:05 +0300

    mainwindow ui changes

--- nodecad/__main__.py
+++ nodecad/__main__.py
@@ -6,8 +6,7 @@ from mainwindow import MainWindow
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow("ribbonbar.png")
-    window.showMaximized()
+    window = MainWindow()
     window.show()
 
     sys.exit(app.exec_())
--- nodecad/mainwindow.py
+++ nodecad/mainwindow.py
@@ -3,7 +3,12 @@ from PySide6.QtWidgets import (
 )
 
 from PySide6.QtGui import (
-    QIcon
+    QIcon,
+    QGuiApplication
+)
+
+from PySide6.QtCore import (
+    QTimer
 )
 
 from pyqtribbon import RibbonBar
@@ -11,22 +16,34 @@ from pyqtribbon import RibbonBar
 from OCC.Display.backend import load_backend
 
 load_backend("pyside6")
-import OCC.Display.qtDisplay as qtDisplay 
+from OCC.Display.qtDisplay import qtViewer3d 
 from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox
 
 class MainWindow(QMainWindow):
-
-    def __init__(self, fileName: str = "shot.jpg", *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("NodeCAD")
+        self.init_ui()
+        
+    def init_ui(self):
         self.set_canvas()
         self.set_ribbonmenu()
-        
+        self.screen_center()
+        QTimer.singleShot(100, self.showMaximized)
+
+    def screen_center(self) -> None:
+        """Centers the window on the screen."""
+        geometry = self.frameGeometry()
+        center = QGuiApplication.primaryScreen().availableGeometry().center()
+        geometry.moveCenter(center)
+        self.move(geometry.topLeft())
+
     def set_canvas(self):
-        self.canvas = qtDisplay.qtViewer3d(self)
+        self.canvas = qtViewer3d(self)
         self.canvas.InitDriver()
         self.display = self.canvas._display
         self.setCentralWidget(self.canvas)
-        
+
     def set_ribbonmenu(self):
         ribbonbar = RibbonBar()
         self.setMenuBar(ribbonbar)
